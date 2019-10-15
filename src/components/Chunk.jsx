29bb6265906197094caa375082baf8ac3cc3fefb
Nicholas Lantz <nlantz@pm.me>
2019-10-15 17:54:31 -0600

    Added proptypes to Chunk component.

--- src/components/Chunk.jsx
+++ src/components/Chunk.jsx
@@ -5,22 +5,32 @@ import Tile from 'Tile';
 const Chunk = ({ onClickTile, id, tiles, sideLength }) => (
     // Could be written better with partition function
     <table>
-    {
-        [...Array(sideLength).keys()].map(i => (
-            <tr>
-              {
-                  tiles.slice(i * sideLength, (i+1) * sideLength).map((t, j) => (
-                      <Tile
-                        key={`${id}-${i*sideLength + j}`}
-                        {...t}
-                        onClick={() => onClickTile(id, i * sideLength + j)}
-                      />  
-                  ))
-              }
-            </tr>
-        ))
-    }
+      {[...Array(sideLength).keys()].map(i => (
+          <tr>
+            {tiles.slice(i * sideLength, (i+1) * sideLength).map((t, j) => (
+                <Tile
+                  key={`${id}-${i*sideLength + j}`}
+                  {...t}
+                  onClick={() => onClickTile(id, i * sideLength + j)}
+                />  
+            ))}
+          </tr>
+      ))}
     </table>
 );
+
+Chunk.propTypes = {
+    onClickTile: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    tiles: PropTypes.arrayOf(
+        PropTypes.shape({
+            isMine: PropTypes.bool.isRequired,
+            owner: PropTypes.number.isRequired,
+            revealed: PropTypes.bool.isRequired,
+            numNeighbors: PropTypes.bool.isRequired,
+        }).isRequired
+    ).isRequired,
+    sideLength: PropTypes.number.isRequired,
+};
 
 export default Chunk;
