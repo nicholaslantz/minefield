f876b4543566be10a2a8f9a3eee0732fd427f790
Nicholas Lantz <nlantz@pm.me>
2019-10-15 18:41:38 -0600

    Added App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import './components/Chunk.jsx';
+
+function App() {
+    // TODO: Render all chunks in store
+    return (
+        <div className="App">
+          
+        </div>
+    );
+}
+
+export default App;
